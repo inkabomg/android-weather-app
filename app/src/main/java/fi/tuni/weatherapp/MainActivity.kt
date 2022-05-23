ec209118e77912bf83c9d1136a2b23d06a69e89f
Inka Lagerbom <77788881+inkabomg@users.noreply.github.com>
2022-05-23 18:29:08 +0300

    Comment out weather icon code

--- app/src/main/java/fi/tuni/weatherapp/MainActivity.kt
+++ app/src/main/java/fi/tuni/weatherapp/MainActivity.kt
@@ -71,11 +71,11 @@ class MainActivity : AppCompatActivity() {
             val sunrise:Long = sys.getLong("sunrise")
             val sunset:Long = sys.getLong("sunset")
 
-            // EI TOIMI
+            /*
             val imageView: ImageView = findViewById(R.id.imageView)
             val icon = weather.getString("icon")
             val iconUrl = "http://openweathermap.org/img/w/"
-            Picasso.with(this).load("$iconUrl$icon.png").into(imageView)
+            Picasso.with(this).load("$iconUrl$icon.png").into(imageView) */
 
             /* Populating extracted data into our textviews */
             findViewById<TextView>(R.id.loc).text = cityName
